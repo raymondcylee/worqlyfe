eeb1e57441556de15257bcc6aae1637442e8c3f5
Shawn Phua <shawn@Shawns-MacBook-Pro.local>
2019-09-11 14:49:35 +0800

    added password

--- models/user.py
+++ models/user.py
@@ -5,8 +5,8 @@ import peewee as pw
 class User(BaseModel):
     name = pw.CharField(unique=False, null=False)
     email = pw.CharField(unique=True, null=False)
-    password = pw.CharField(null=False)
     role = pw.CharField(null=False)
+    password = pw.CharField(null=False)
     department = pw.CharField(null=False)
     profile_picture = pw.CharField(null=True)
     is_manager = pw.BooleanField(null=True)
