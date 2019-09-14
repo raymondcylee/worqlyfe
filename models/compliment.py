d9cfa250ee2f62b60531c26dabeeb57af79175ef
Shawn Phua <1998shawnmarch@gmail.com>
2019-09-14 11:37:26 +0800

    Changed user to current_user

--- models/compliment.py
+++ models/compliment.py
@@ -8,5 +8,5 @@ class Compliment(BaseModel):
     compliment = pw.CharField(unique=False, null=False)
     sender = pw.ForeignKeyField(User, backref='recipients')
     recipient = pw.ForeignKeyField(User, backref='senders')
-    badge = pw.ForeignKeyField(Badge, backref='badge')
+    badge = pw.ForeignKeyField(Badge, backref='badges')
 
