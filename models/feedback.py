109db05a7ee20d858570cc5af264508ad47f87e1
Shawn Phua <1998shawnmarch@gmail.com>
2019-09-18 12:23:03 +0800

    Completed request feedback form

---
+++ models/feedback.py
@@ -0,0 +1,11 @@
+from models.base_model import BaseModel
+from models.user import User
+import peewee as pw
+
+
+class Feedback(BaseModel):
+    message = pw.CharField(null=False)
+    subject = pw.CharField(null=False)
+    answered = pw.BooleanField(null=False)
+    requester = pw.ForeignKeyField(User, backref='receivers')
+    receiver = pw.ForeignKeyField(User, backref='requesters') # current_user.requesters means we are looking for current_user == receiver
